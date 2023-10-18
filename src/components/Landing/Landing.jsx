7b3c72c8639a338bed4525e440cf586a382259c2
Sumant-Testbook <sumant.dusane@testbook.com>
2023-10-18 09:34:45 +0530

    initial setup

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import productImage from "./../../assets/images/product-image.jpeg";
+import "./landing.scss";
+
+export default function Landing() {
+    return (
+        <div className='landing'>
+            <div className="navbar">
+                <a href="#home"><h2 className='navbar__logo'>AVALAUNCH.ai</h2></a>
+                <ul className="navbar__navs">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#features">Features</a></li>
+                    <li><a href="#dashboard">Dashboard</a></li>
+                    <li className='btn btn-primary--small'><a href="#dashboard">Login</a></li>
+                </ul>
+            </div>
+            <div className="hero" id='hero'>
+                <div className="hero__left">
+                    <h1 className='hero__left__title'>AVALAUNCH.ai</h1>
+                    <h2 className='hero__left__subtitle'>Manage your business Easier and Simpler</h2>
+                    <p className='hero__left__desc'>The easiest way to help organize and control your business with fast service using Ai</p>
+                </div>
+                <img className='hero__right' src={productImage} alt="Ava Launch" />
+            </div>
+            <div className="features" id='features'>
+                <div className="card">
+                    <div className="card__icon"><i class="fas fa-hand-holding-usd"/></div>
+                    <div className="card__title">Month Wise Sales Report</div>
+                </div>
+                <div className="card">
+                    <div className="card__icon"><i class="fas fa-user-secret"/></div>
+                    <div className="card__title">Check your Surrounding</div>
+                </div>
+                <div className="card">
+                    <div className="card__icon"><i class="fas fa-project-diagram"/></div>
+                    <div className="card__title">ML will tell you the best month to launch</div>
+                </div>
+                <div className="card">
+                    <div className="card__icon"><i className="fas fa-rocket"/></div>
+                    <div className="card__title">Boost Your Sales upto 60%</div>
+                </div>
+            </div>
+            <div className="cta" id='cta'>
+                <h3>Try our software</h3>
+                <button className='btn btn-primary'>Do to Dashboard</button>
+            </div>
+            <div className="landing__clients"></div>
+        </div>
+    )
+}
